d553943dbb4d2d43591d8b8138694c8f973a8e70
Fernando0900 <jcadillo888@gmail.com>
2025-06-22 21:33:52 -0500

    Corrige BASE_URL en sitemap para producción

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="5e6f32efce2ea4bc" />
       </Head>
       <Navbar />
       <Component {...pageProps} />
--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://notafix.vercel.app";
 
 async function obtenerRutasDesdeBD() {
   return [
